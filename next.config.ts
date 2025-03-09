ccf03420e94ad88191cf3f9ea1b2eca37480ea28
Milind Sharma <milindsharma8@gmail.com>
2025-03-09 17:22:26 +0800

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    reactStrictMode: true,
+    images: {
+        domains: [], // Add any image domains you'll use
+    },
 };
 
 export default nextConfig;
+
+// next.config.js
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+});
+
+module.exports = withMDX(nextConfig);
